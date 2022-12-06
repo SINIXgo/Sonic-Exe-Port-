c39ac3239aa9043790e6a063f9a85a152e209a22
SINIXgo <116418436+SINIXgo@users.noreply.github.com>
2022-12-06 19:41:58 -0300

    Add files via upload

---
+++ assets/preload/data/red-ring/give up.lua
@@ -0,0 +1,40 @@
+function onGameOverStart()
+    setPropertyFromClass('GameOverSubstate', 'give up touls', 'f');
+    
+	makeLuaText('CountDown', '10', 650, 300, 30)
+	setTextAlignment('CountDown', 'center')
+	setObjectCamera('CountDown', 'other')
+	setTextSize('CountDown', 70)
+	addLuaText('CountDown')
+
+    runTimer('CountDown', 1, 10)
+end
+
+function onTimerCompleted(d, loops, loopsleft)
+    if d == 'CountDown' then
+        if loopsleft == 9 then
+            setTextString('CountDown', '9')
+        elseif loopsleft == 8 then
+            setTextString('CountDown', '8')
+        elseif loopsleft == 7 then
+            setTextString('CountDown', '7')
+        elseif loopsleft == 6 then
+            setTextString('CountDown', '6')
+        elseif loopsleft == 5 then
+            setTextString('CountDown', '5')
+        elseif loopsleft == 4 then
+            setTextString('CountDown', '4')
+        elseif loopsleft == 3 then
+            setTextString('CountDown', '3')
+            setTextColor('CountDown', '0xFFEB4034')
+        elseif loopsleft == 2 then
+            setTextString('CountDown', '2')
+        elseif loopsleft == 1 then
+            setTextString('CountDown', '1')
+        elseif loopsleft == 0 then
+            setTextString('CountDown', '0')
+            loadSong('cry-hill', difficulty)
+        end
+    end
+end
+
