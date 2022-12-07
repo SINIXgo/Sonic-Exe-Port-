ad9f72310e19e7296b366c3cf3fc704bbccaef8b
SINIXgo <116418436+SINIXgo@users.noreply.github.com>
2022-12-07 00:31:51 -0300

    Add files via upload

---
+++ assets/preload/data/demonized/script.lua
@@ -0,0 +1,5 @@
+function onCreate() 
+makeLuaText('watermark'," Ported By Sinix Go and mateuzinho  ", 0, 2, 688); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 20);
+    addLuaText('watermark');
+end
